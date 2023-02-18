4268633f989a1fb8e013e9c51198e9be06008a66
Hong-She Liang <sorn@taorules.com>
2023-02-18 21:22:33 +0800

    Turn all command as function
    So we get a function like intelligent supports

--- pytosim/api/cmds.py
+++ pytosim/api/cmds.py
@@ -2,281 +2,1114 @@
 
 # User-level operation that control the Source Insight
 
-About_Source_Insight = 0
-Activate_Menu_Commands = 0
-Activate_Global_Symbol_List = 0
-Activate_Relation_Window = 0
-Activate_Search_Results = 0
-Activate_Symbol_Window = 0
-Add_and_Remove_Project_Files = 0
-Add_File = 0
-Add_File_List = 0
-Advanced_Options = 0
-Back_Tab = 0
-Backspace = 0
-Beginning_of_Line = 0
-Beginning_of_Selection = 0
-Blank_Line_Down = 0
-Blank_Line_Up = 0
-Block_Down = 0
-Block_Up = 0
-Bookmark = 0
-Bottom_of_File = 0
-Bottom_of_Window = 0
-Browse_Files = 0
-Browse_Project_Symbols = 0
-Browse_Global_Symbols_Dialog_box = 0
-Browse_Local_File_Symbols = 0
-Cascade_Windows = 0
-Checkpoint = 0
-Checkpoint_All = 0
-Clear_Highlights = 0
-Clip_Properties = 0
-Clip_Window_Properties = 0
-Close = 0
-Close_All = 0
-Close_Project = 0
-Close_Window = 0
-Color_Options = 0
-Command_Shell = 0
-Complete_Symbol = 0
-Context_Window = 0
-Context_Window_Properties = 0
-Copy = 0
-Copy_Line = 0
-Copy_Line_Right = 0
-Copy_List = 0
-Copy_Symbol = 0
-Copy_To_Clip = 0
-Create_Key_List = 0
-Create_Command_List = 0
-Cursor_Down = 0
-Cursor_Left = 0
-Cursor_Right = 0
-Cursor_Up = 0
-Custom_Commands = 0
-Cut = 0
-Cut_Line = 0
-Cut_Line_Left = 0
-Cut_Line_Right = 0
-Cut_Selection_or_Paste = 0
-Cut_Symbol = 0
-Cut_To_Clip = 0
-Cut_Word = 0
-Cut_Word_Left = 0
-Delete = 0
-Delete_All_Clips = 0
-Delete_Character = 0
-Delete_Clip = 0
-Delete_File = 0
-Delete_Line = 0
-Display_Options = 0
-Document_Options = 0
-Draft_View = 0
-Drag_Line_Down = 0
-Drag_Line_Down_More = 0
-Drag_Line_Up = 0
-Drag_Line_Up_More = 0
-Duplicate = 0
-Duplicate_Symbol = 0
-Edit_Condition = 0
-Enable_Event_Handlers = 0
-End_of_Line = 0
-End_of_Selection = 0
-Exit = 0
-Exit_and_Suspend = 0
-Expand_Special = 0
-File_Options = 0
-Folder_Options = 0
-Function_Down = 0
-Function_Up = 0
-General_Options = 0
-Go_Back = 0
-Go_Back_Toggle = 0
-Go_Forward = 0
-Go_To_First_Link = 0
-Go_To_Line = 0
-Go_To_Next_Change = 0
-Go_To_Previous_Change = 0
-Go_To_Next_Link = 0
-Go_To_Previous_Link = 0
-Help = 0
-Help_Mode = 0
-Highlight_Word = 0
-Incremental_Search = 0
-Incremental_Search_Mode = 0
-Incremental_Search_Backward = 0
-Horizontal_Scroll_Bar = 0
-HTML_Help = 0
-Indent_Left = 0
-Indent_Right = 0
-Insert_ASCII = 0
-Insert_File = 0
-Insert_Line = 0
-Insert_Line_Before_Next = 0
-Insert_New_Line = 0
-Join_Lines = 0
-Jump_To_Base_Type = 0
-Jump_To_Caller = 0
-Jump_To_Definition = 0
-Jump_To_Link = 0
-Jump_To_Prototype = 0
-Key_Assignments = 0
-Keyword_List = 0
-Language_Options = 0
-Language_Properties = 0
-Line_Numbers = 0
-Link_All_Windows = 0
-Link_Window = 0
-Load_Configuration = 0
-Load_File = 0
-Load_Search_String = 0
-Lock_Context_Window = 0
-Lock_Relation_Window = 0
-Lookup_References = 0
-Make_Column_Selection = 0
-Menu_Assignments = 0
-New = 0
-New_Clip = 0
-New_Relation_Window = 0
-New_Project = 0
-New_Window = 0
-Next_File = 0
-Next_Relation_Window_View = 0
-Open = 0
-Open_Project = 0
-Page_Down = 0
-Page_Setup = 0
-Page_Up = 0
-Paren_Left = 0
-Paren_Right = 0
-Parse_Source_Links = 0
-Paste = 0
-Paste_From_Clip = 0
-Paste_Line = 0
-Paste_Symbol = 0
-Play_Recording = 0
-Preferences = 0
-Print = 0
-Print_Relation_Window = 0
-Project_Document_Types = 0
-Project_File_Browser = 0
-Project_File_List = 0
-Project_Symbol_Classes = 0
-Project_Symbol_List = 0
-Project_Window_Properties = 0
-Project_Settings = 0
-Project_Report = 0
-Project_Window_command = 0
-Rebuild_Project = 0
-Record_New_Default_Properties = 0
-Redo = 0
-Redo_All = 0
-Redraw_Screen = 0
-Reform_Paragraph = 0
-Refresh_Relation_Window = 0
-Relation_Graph_Properties = 0
-Relation_Window = 0
-Relation_Window_Properties = 0
-Relation_Window_Properties_Dialog_Box = 0
-Reload_File = 0
-Reload_Modified_Files = 0
-Remove_File = 0
-Remove_Project = 0
-Remote_Options = 0
-Rename = 0
-Renumber = 0
-Repeat_Typing = 0
-Replace = 0
-Replace_Files = 0
-Restore_File = 0
-Restore_Lines = 0
-Save = 0
-Save_A_Copy = 0
-Save_All = 0
-Save_All_Quietly = 0
-Save_As = 0
-Save_Configuration = 0
-Save_Selection = 0
-Save_Workspace = 0
-Scroll_Half_Page_Down = 0
-Scroll_Half_Page_Up = 0
-Scroll_Left = 0
-Scroll_Line_Down = 0
-Scroll_Line_Up = 0
-Scroll_Right = 0
-SDK_Help = 0
-Search = 0
-Search_Backward = 0
-Search_Backward_for_Selection = 0
-Search_Files = 0
-Search_Forward = 0
-Search_Forward_for_Selection = 0
-Search_List = 0
-Search_Project = 0
-Searching_Options = 0
-Select_All = 0
-Select_Block = 0
-Select_Char_Left = 0
-Select_Char_Right = 0
-Select_Function_or_Symbol = 0
-Select_Line = 0
-Select_Line_Down = 0
-Select_Line_Up = 0
-Select_Match = 0
-Select_Next_Window = 0
-Select_Sentence = 0
-Select_Symbol = 0
-Select_To = 0
-Select_To_End_Of_File = 0
-Select_To_Top_Of_File = 0
-Select_Word = 0
-Select_Word_Left = 0
-Select_Word_Right = 0
-Selection_History = 0
-Setup_Common_Projects = 0
-Setup_HTML_Help = 0
-Setup_WinHelp_File = 0
-Show_Clipboard = 0
-Show_File_Status = 0
-Simple_Tab = 0
-Smart_End_of_Line = 0
-Smart_Beginning_of_Line = 0
-Smart_Rename = 0
-Smart_Tab = 0
-Sort_Symbol_Window = 0
-Sort_Symbols_By_Line = 0
-Sort_Symbols_by_Name = 0
-Sort_Symbols_By_Type = 0
-Source_Dynamics_on_the_Web = 0
-Start_Recording = 0
-Stop_Recording = 0
-Style_Properties = 0
-Symbol_Info = 0
-Symbol_Lookup_Options = 0
-Symbol_Window_command = 0
-Symbol_Window_Properties = 0
-Sync_File_Windows = 0
-Synchronize_Files = 0
-Syntax_Decorations = 0
-Syntax_Formatting = 0
-Tile_Horizontal = 0
-Tile_One_Window = 0
-Tile_Two_Windows = 0
-Tile_Vertical = 0
-Toggle_Insert_Mode = 0
-Top_of_File = 0
-Top_of_Window = 0
-Touch_All_Files_in_Relation = 0
-Typing_Options = 0
-Undo = 0
-Undo_All = 0
-Vertical_Scroll_Bar = 0
-View_Clip = 0
-View_Relation_Outline = 0
-View_Relation_Window_Horizontal_Graph = 0
-View_Relation_Window_Vertical_Graph = 0
-Window_List = 0
-Word_Left = 0
-Word_Right = 0
-Zoom_Window = 0
+
+def About_Source_Insight():
+    pass
+
+
+def Activate_Menu_Commands():
+    pass
+
+
+def Activate_Global_Symbol_List():
+    pass
+
+
+def Activate_Relation_Window():
+    pass
+
+
+def Activate_Search_Results():
+    pass
+
+
+def Activate_Symbol_Window():
+    pass
+
+
+def Add_and_Remove_Project_Files():
+    pass
+
+
+def Add_File():
+    pass
+
+
+def Add_File_List():
+    pass
+
+
+def Advanced_Options():
+    pass
+
+
+def Back_Tab():
+    pass
+
+
+def Backspace():
+    pass
+
+
+def Beginning_of_Line():
+    pass
+
+
+def Beginning_of_Selection():
+    pass
+
+
+def Blank_Line_Down():
+    pass
+
+
+def Blank_Line_Up():
+    pass
+
+
+def Block_Down():
+    pass
+
+
+def Block_Up():
+    pass
+
+
+def Bookmark():
+    pass
+
+
+def Bottom_of_File():
+    pass
+
+
+def Bottom_of_Window():
+    pass
+
+
+def Browse_Files():
+    pass
+
+
+def Browse_Project_Symbols():
+    pass
+
+
+def Browse_Global_Symbols_Dialog_box():
+    pass
+
+
+def Browse_Local_File_Symbols():
+    pass
+
+
+def Cascade_Windows():
+    pass
+
+
+def Checkpoint():
+    pass
+
+
+def Checkpoint_All():
+    pass
+
+
+def Clear_Highlights():
+    pass
+
+
+def Clip_Properties():
+    pass
+
+
+def Clip_Window_Properties():
+    pass
+
+
+def Close():
+    pass
+
+
+def Close_All():
+    pass
+
+
+def Close_Project():
+    pass
+
+
+def Close_Window():
+    pass
+
+
+def Color_Options():
+    pass
+
+
+def Command_Shell():
+    pass
+
+
+def Complete_Symbol():
+    pass
+
+
+def Context_Window():
+    pass
+
+
+def Context_Window_Properties():
+    pass
+
+
+def Copy():
+    pass
+
+
+def Copy_Line():
+    pass
+
+
+def Copy_Line_Right():
+    pass
+
+
+def Copy_List():
+    pass
+
+
+def Copy_Symbol():
+    pass
+
+
+def Copy_To_Clip():
+    pass
+
+
+def Create_Key_List():
+    pass
+
+
+def Create_Command_List():
+    pass
+
+
+def Cursor_Down():
+    pass
+
+
+def Cursor_Left():
+    pass
+
+
+def Cursor_Right():
+    pass
+
+
+def Cursor_Up():
+    pass
+
+
+def Custom_Commands():
+    pass
+
+
+def Cut():
+    pass
+
+
+def Cut_Line():
+    pass
+
+
+def Cut_Line_Left():
+    pass
+
+
+def Cut_Line_Right():
+    pass
+
+
+def Cut_Selection_or_Paste():
+    pass
+
+
+def Cut_Symbol():
+    pass
+
+
+def Cut_To_Clip():
+    pass
+
+
+def Cut_Word():
+    pass
+
+
+def Cut_Word_Left():
+    pass
+
+
+def Delete():
+    pass
+
+
+def Delete_All_Clips():
+    pass
+
+
+def Delete_Character():
+    pass
+
+
+def Delete_Clip():
+    pass
+
+
+def Delete_File():
+    pass
+
+
+def Delete_Line():
+    pass
+
+
+def Display_Options():
+    pass
+
+
+def Document_Options():
+    pass
+
+
+def Draft_View():
+    pass
+
+
+def Drag_Line_Down():
+    pass
+
+
+def Drag_Line_Down_More():
+    pass
+
+
+def Drag_Line_Up():
+    pass
+
+
+def Drag_Line_Up_More():
+    pass
+
+
+def Duplicate():
+    pass
+
+
+def Duplicate_Symbol():
+    pass
+
+
+def Edit_Condition():
+    pass
+
+
+def Enable_Event_Handlers():
+    pass
+
+
+def End_of_Line():
+    pass
+
+
+def End_of_Selection():
+    pass
+
+
+def Exit():
+    pass
+
+
+def Exit_and_Suspend():
+    pass
+
+
+def Expand_Special():
+    pass
+
+
+def File_Options():
+    pass
+
+
+def Folder_Options():
+    pass
+
+
+def Function_Down():
+    pass
+
+
+def Function_Up():
+    pass
+
+
+def General_Options():
+    pass
+
+
+def Go_Back():
+    pass
+
+
+def Go_Back_Toggle():
+    pass
+
+
+def Go_Forward():
+    pass
+
+
+def Go_To_First_Link():
+    pass
+
+
+def Go_To_Line():
+    pass
+
+
+def Go_To_Next_Change():
+    pass
+
+
+def Go_To_Previous_Change():
+    pass
+
+
+def Go_To_Next_Link():
+    pass
+
+
+def Go_To_Previous_Link():
+    pass
+
+
+def Help():
+    pass
+
+
+def Help_Mode():
+    pass
+
+
+def Highlight_Word():
+    pass
+
+
+def Incremental_Search():
+    pass
+
+
+def Incremental_Search_Mode():
+    pass
+
+
+def Incremental_Search_Backward():
+    pass
+
+
+def Horizontal_Scroll_Bar():
+    pass
+
+
+def HTML_Help():
+    pass
+
+
+def Indent_Left():
+    pass
+
+
+def Indent_Right():
+    pass
+
+
+def Insert_ASCII():
+    pass
+
+
+def Insert_File():
+    pass
+
+
+def Insert_Line():
+    pass
+
+
+def Insert_Line_Before_Next():
+    pass
+
+
+def Insert_New_Line():
+    pass
+
+
+def Join_Lines():
+    pass
+
+
+def Jump_To_Base_Type():
+    pass
+
+
+def Jump_To_Caller():
+    pass
+
+
+def Jump_To_Definition():
+    pass
+
+
+def Jump_To_Link():
+    pass
+
+
+def Jump_To_Prototype():
+    pass
+
+
+def Key_Assignments():
+    pass
+
+
+def Keyword_List():
+    pass
+
+
+def Language_Options():
+    pass
+
+
+def Language_Properties():
+    pass
+
+
+def Line_Numbers():
+    pass
+
+
+def Link_All_Windows():
+    pass
+
+
+def Link_Window():
+    pass
+
+
+def Load_Configuration():
+    pass
+
+
+def Load_File():
+    pass
+
+
+def Load_Search_String():
+    pass
+
+
+def Lock_Context_Window():
+    pass
+
+
+def Lock_Relation_Window():
+    pass
+
+
+def Lookup_References():
+    pass
+
+
+def Make_Column_Selection():
+    pass
+
+
+def Menu_Assignments():
+    pass
+
+
+def New():
+    pass
+
+
+def New_Clip():
+    pass
+
+
+def New_Relation_Window():
+    pass
+
+
+def New_Project():
+    pass
+
+
+def New_Window():
+    pass
+
+
+def Next_File():
+    pass
+
+
+def Next_Relation_Window_View():
+    pass
+
+
+def Open():
+    pass
+
+
+def Open_Project():
+    pass
+
+
+def Page_Down():
+    pass
+
+
+def Page_Setup():
+    pass
+
+
+def Page_Up():
+    pass
+
+
+def Paren_Left():
+    pass
+
+
+def Paren_Right():
+    pass
+
+
+def Parse_Source_Links():
+    pass
+
+
+def Paste():
+    pass
+
+
+def Paste_From_Clip():
+    pass
+
+
+def Paste_Line():
+    pass
+
+
+def Paste_Symbol():
+    pass
+
+
+def Play_Recording():
+    pass
+
+
+def Preferences():
+    pass
+
+
+def Print():
+    pass
+
+
+def Print_Relation_Window():
+    pass
+
+
+def Project_Document_Types():
+    pass
+
+
+def Project_File_Browser():
+    pass
+
+
+def Project_File_List():
+    pass
+
+
+def Project_Symbol_Classes():
+    pass
+
+
+def Project_Symbol_List():
+    pass
+
+
+def Project_Window_Properties():
+    pass
+
+
+def Project_Settings():
+    pass
+
+
+def Project_Report():
+    pass
+
+
+def Project_Window_command():
+    pass
+
+
+def Rebuild_Project():
+    pass
+
+
+def Record_New_Default_Properties():
+    pass
+
+
+def Redo():
+    pass
+
+
+def Redo_All():
+    pass
+
+
+def Redraw_Screen():
+    pass
+
+
+def Reform_Paragraph():
+    pass
+
+
+def Refresh_Relation_Window():
+    pass
+
+
+def Relation_Graph_Properties():
+    pass
+
+
+def Relation_Window():
+    pass
+
+
+def Relation_Window_Properties():
+    pass
+
+
+def Relation_Window_Properties_Dialog_Box():
+    pass
+
+
+def Reload_File():
+    pass
+
+
+def Reload_Modified_Files():
+    pass
+
+
+def Remove_File():
+    pass
+
+
+def Remove_Project():
+    pass
+
+
+def Remote_Options():
+    pass
+
+
+def Rename():
+    pass
+
+
+def Renumber():
+    pass
+
+
+def Repeat_Typing():
+    pass
+
+
+def Replace():
+    pass
+
+
+def Replace_Files():
+    pass
+
+
+def Restore_File():
+    pass
+
+
+def Restore_Lines():
+    pass
+
+
+def Save():
+    pass
+
+
+def Save_A_Copy():
+    pass
+
+
+def Save_All():
+    pass
+
+
+def Save_All_Quietly():
+    pass
+
+
+def Save_As():
+    pass
+
+
+def Save_Configuration():
+    pass
+
+
+def Save_Selection():
+    pass
+
+
+def Save_Workspace():
+    pass
+
+
+def Scroll_Half_Page_Down():
+    pass
+
+
+def Scroll_Half_Page_Up():
+    pass
+
+
+def Scroll_Left():
+    pass
+
+
+def Scroll_Line_Down():
+    pass
+
+
+def Scroll_Line_Up():
+    pass
+
+
+def Scroll_Right():
+    pass
+
+
+def SDK_Help():
+    pass
+
+
+def Search():
+    pass
+
+
+def Search_Backward():
+    pass
+
+
+def Search_Backward_for_Selection():
+    pass
+
+
+def Search_Files():
+    pass
+
+
+def Search_Forward():
+    pass
+
+
+def Search_Forward_for_Selection():
+    pass
+
+
+def Search_List():
+    pass
+
+
+def Search_Project():
+    pass
+
+
+def Searching_Options():
+    pass
+
+
+def Select_All():
+    pass
+
+
+def Select_Block():
+    pass
+
+
+def Select_Char_Left():
+    pass
+
+
+def Select_Char_Right():
+    pass
+
+
+def Select_Function_or_Symbol():
+    pass
+
+
+def Select_Line():
+    pass
+
+
+def Select_Line_Down():
+    pass
+
+
+def Select_Line_Up():
+    pass
+
+
+def Select_Match():
+    pass
+
+
+def Select_Next_Window():
+    pass
+
+
+def Select_Sentence():
+    pass
+
+
+def Select_Symbol():
+    pass
+
+
+def Select_To():
+    pass
+
+
+def Select_To_End_Of_File():
+    pass
+
+
+def Select_To_Top_Of_File():
+    pass
+
+
+def Select_Word():
+    pass
+
+
+def Select_Word_Left():
+    pass
+
+
+def Select_Word_Right():
+    pass
+
+
+def Selection_History():
+    pass
+
+
+def Setup_Common_Projects():
+    pass
+
+
+def Setup_HTML_Help():
+    pass
+
+
+def Setup_WinHelp_File():
+    pass
+
+
+def Show_Clipboard():
+    pass
+
+
+def Show_File_Status():
+    pass
+
+
+def Simple_Tab():
+    pass
+
+
+def Smart_End_of_Line():
+    pass
+
+
+def Smart_Beginning_of_Line():
+    pass
+
+
+def Smart_Rename():
+    pass
+
+
+def Smart_Tab():
+    pass
+
+
+def Sort_Symbol_Window():
+    pass
+
+
+def Sort_Symbols_By_Line():
+    pass
+
+
+def Sort_Symbols_by_Name():
+    pass
+
+
+def Sort_Symbols_By_Type():
+    pass
+
+
+def Source_Dynamics_on_the_Web():
+    pass
+
+
+def Start_Recording():
+    pass
+
+
+def Stop_Recording():
+    pass
+
+
+def Style_Properties():
+    pass
+
+
+def Symbol_Info():
+    pass
+
+
+def Symbol_Lookup_Options():
+    pass
+
+
+def Symbol_Window_command():
+    pass
+
+
+def Symbol_Window_Properties():
+    pass
+
+
+def Sync_File_Windows():
+    pass
+
+
+def Synchronize_Files():
+    pass
+
+
+def Syntax_Decorations():
+    pass
+
+
+def Syntax_Formatting():
+    pass
+
+
+def Tile_Horizontal():
+    pass
+
+
+def Tile_One_Window():
+    pass
+
+
+def Tile_Two_Windows():
+    pass
+
+
+def Tile_Vertical():
+    pass
+
+
+def Toggle_Insert_Mode():
+    pass
+
+
+def Top_of_File():
+    pass
+
+
+def Top_of_Window():
+    pass
+
+
+def Touch_All_Files_in_Relation():
+    pass
+
+
+def Typing_Options():
+    pass
+
+
+def Undo():
+    pass
+
+
+def Undo_All():
+    pass
+
+
+def Vertical_Scroll_Bar():
+    pass
+
+
+def View_Clip():
+    pass
+
+
+def View_Relation_Outline():
+    pass
+
+
+def View_Relation_Window_Horizontal_Graph():
+    pass
+
+
+def View_Relation_Window_Vertical_Graph():
+    pass
+
+
+def Window_List():
+    pass
+
+
+def Word_Left():
+    pass
+
+
+def Word_Right():
+    pass
+
+
+def Zoom_Window():
+    pass
