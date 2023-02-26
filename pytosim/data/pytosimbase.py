1cf207d463c0bcdebb5683798545874786591bb8
Hong-She Liang <sorn@taorules.com>
2023-02-26 23:01:45 +0800

    The string mid method changed to accept negative values

--- pytosim/data/pytosimbase.py
+++ pytosim/data/pytosimbase.py
@@ -16,7 +16,28 @@ def _pytosim_max(a, b) -> float:
     return b
 
 
-def _pytosim_strmid(s: str, idx_from: int, idx_to: int) -> str:
-    idx_from = _pytosim_min(idx_from, len(s))
-    idx_to = _pytosim_max(idx_to, idx_from)
+def _pytosim_normalize_idx(len: int, idx: int) -> int:
+    """Normalize the index value"""
+
+    if idx < 0:
+        if idx < -len:
+            idx = -len
+
+        idx = len + idx
+    elif idx > len:
+        # idx use as stop index could after the array's end
+        idx = len
+
+    return idx
+
+
+def _pytosim_mid(s: str, idx_from: int, idx_to: int) -> str:
+    s_len = len(s)
+    if s_len <= 0:
+        return ""
+
+    idx_from = _pytosim_normalize_idx(s_len, idx_from)
+    idx_from = _pytosim_min(idx_from, s_len - 1)
+    idx_to = _pytosim_normalize_idx(s_len, idx_to)
+    idx_to = _pytosim_max(idx_to, idx_from + 1)
     return simstr.strmid(s, idx_from, idx_to)
